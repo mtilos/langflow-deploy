651f3830d5edaf37159a5babbfe1efe3b9157b8f
mtilos <mtilos71@gmail.com>
2025-07-02 17:41:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import os
 from langflow import run
 
 if __name__ == "__main__":
-    run(host="0.0.0.0", port=7860)
+    port = int(os.environ.get("PORT", 7860))
+    run(host="0.0.0.0", port=port)
+
